fa883cab7a2b8bd69421e38fd23993ce46ebb096
Marek Olsak <m_olsak@utb.cz>
2023-01-04 21:57:10 +0100

    Manage carousel items searching implemented

--- Blazor.Presentation.Client/Services/CarouselItemService.cs
+++ Blazor.Presentation.Client/Services/CarouselItemService.cs
@@ -34,13 +34,17 @@ public sealed class CarouselItemService
             .Handle<HttpRequestException>()
             .WaitAndRetryAsync(Constants.MaxHttpRequestRetries,times => TimeSpan.FromMilliseconds(times * 100));
 
-        /*return await retryPolicy.ExecuteAndCaptureAsync(async () => await httpClient.GetFromJsonAsync<List<Entity>>(
-            $"CarouselItem?pageNumber={carouselItemParameters.PageNumber}&pageSize={carouselItemParameters.PageSize}"));*/
-        
         return await retryPolicy.ExecuteAndCaptureAsync(async () =>
         {
-            var httpResponse = await httpClient.GetAsync(
-                $"CarouselItem?pageNumber={carouselItemParameters.PageNumber}&pageSize={carouselItemParameters.PageSize}");
+            var requestUri = String.Join('&', "CarouselItem?",
+                $"pageNumber={carouselItemParameters.PageNumber}",
+                $"pageSize={carouselItemParameters.PageSize}",
+                $"searchTerm={carouselItemParameters.SearchTerm}",
+                $"minId={carouselItemParameters.MinId}",
+                $"maxId={carouselItemParameters.MaxId}",
+                $"orderBy={carouselItemParameters.OrderBy}");
+            
+            var httpResponse = await httpClient.GetAsync(requestUri);
 
             return httpResponse.IsSuccessStatusCode 
                 ? await httpResponse.Content.ReadFromJsonAsync<List<Entity>>() 
@@ -48,138 +52,6 @@ public sealed class CarouselItemService
         });
     }
 
-    [Obsolete]
-    public async ValueTask<ResponseDetails<LinkCollectionWrapper<Entity>>> GetAllWithLinksAsync(
-        CarouselItemParameters carouselItemParameters)
-    {
-        var responseDetails = new ResponseDetails<LinkCollectionWrapper<Entity>> { Content = new LinkCollectionWrapper<Entity>() };
-        
-        var httpClient = _httpClientFactory.CreateClient("Default");
-        httpClient.DefaultRequestHeaders.Accept.Clear();
-        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.utb.hateoas+json"));
-        
-        var retryPolicy = Policy<ResponseDetails<LinkCollectionWrapper<Entity>>>
-            .Handle<HttpRequestException>()
-            .WaitAndRetryAsync(Constants.MaxHttpRequestRetries,times => TimeSpan.FromMilliseconds(times * 100));
-
-        return await retryPolicy.ExecuteAsync(async () =>
-        {
-            try
-            {
-                var response = await httpClient.GetAsync(
-                    $"CarouselItem?pageNumber={carouselItemParameters.PageNumber}&pageSize={carouselItemParameters.PageSize}",
-                    HttpCompletionOption.ResponseHeadersRead);
-
-                responseDetails.IsResponse = true;
-                if (response.IsSuccessStatusCode)
-                {
-                    responseDetails.IsSuccess = true;
-                }
-                else
-                {
-                    responseDetails.Message = "Http request has not been successful";
-                    return responseDetails;
-                }
-
-                if (response.Content is object &&
-                    response.Content.Headers.ContentType?.MediaType == "application/vnd.utb.hateoas+json")
-                {
-                    var contentStream = await response.Content.ReadAsStreamAsync();
-
-                    responseDetails.Content =
-                        await System.Text.Json.JsonSerializer.DeserializeAsync<LinkCollectionWrapper<Entity>>(
-                            contentStream,
-                            new System.Text.Json.JsonSerializerOptions
-                            {
-                                PropertyNameCaseInsensitive = true,
-                                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-                            });
-                }
-                else
-                {
-                    responseDetails.Message = "Invalid content media type";
-                }
-            }
-            catch (TaskCanceledException )
-            {
-                responseDetails.Message = "Http client connection timeout failure";
-            }
-            catch (JsonReaderException)
-            {
-                responseDetails.Message = "Json deserialization failure";
-            }
-
-            return responseDetails;
-        });
-    }
-    
-    [Obsolete]
-    public async ValueTask<ResponseDetails<LinkCollectionWrapper<Entity>>> GetWithLinksAsync(int carouselItemId,
-        CarouselItemParameters carouselItemParameters)
-    {
-        var responseDetails = new ResponseDetails<LinkCollectionWrapper<Entity>> { IsResponse = true, Content = new LinkCollectionWrapper<Entity>() };
-        
-        var httpClient = _httpClientFactory.CreateClient("Default");
-        httpClient.DefaultRequestHeaders.Accept.Clear();
-        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.utb.hateoas+json"));
-        
-        var retryPolicy = Policy<ResponseDetails<LinkCollectionWrapper<Entity>>>
-            .Handle<HttpRequestException>()
-            .WaitAndRetryAsync(Constants.MaxHttpRequestRetries,times => TimeSpan.FromMilliseconds(times * 100));
-
-        return await retryPolicy.ExecuteAsync(async () =>
-        {
-            try
-            {
-                var response = await httpClient.GetAsync(
-                    $"CarouselItem/{carouselItemId}?pageNumber={carouselItemParameters.PageNumber}&pageSize={carouselItemParameters.PageSize}",
-                    HttpCompletionOption.ResponseHeadersRead);
-
-                responseDetails.IsResponse = true;
-                if (response.IsSuccessStatusCode)
-                {
-                    responseDetails.IsSuccess = true;
-                }
-                else
-                {
-                    responseDetails.Message = "Http request has not been successful";
-                    return responseDetails;
-                }
-
-                if (response.Content is object &&
-                    response.Content.Headers.ContentType?.MediaType == "application/vnd.utb.hateoas+json")
-                {
-                    var contentStream = await response.Content.ReadAsStreamAsync();
-
-                    responseDetails.Content =
-                        await System.Text.Json.JsonSerializer.DeserializeAsync<LinkCollectionWrapper<Entity>>(
-                            contentStream,
-                            new System.Text.Json.JsonSerializerOptions
-                            {
-                                PropertyNameCaseInsensitive = true,
-                                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-                            });
-                }
-                else
-                {
-                    responseDetails.Message = "Invalid content media type";
-                    return responseDetails;
-                }
-            }
-            catch (TaskCanceledException )
-            {
-                responseDetails.Message = "Http client connection timeout failure";
-                responseDetails.IsResponse = false;
-            }
-            catch (JsonReaderException)
-            {
-                responseDetails.Message = "Json deserialization failure";
-            }
-            
-            return responseDetails;
-        });
-    }
-    
     public async Task<PolicyResult<HttpResponseMessage>> DeleteAsync(int carouselItemId)
     {
         var httpClient = _httpClientFactory.CreateClient("Default");
