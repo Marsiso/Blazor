d2b9878ffee1ad037125f64031677bf305bfa444
Marek Olsak <m_olsak@utb.cz>
2023-01-05 02:25:28 +0100

    Image file upload implemented

--- Blazor.Presentation.Client/Services/ImageService.cs
+++ Blazor.Presentation.Client/Services/ImageService.cs
@@ -1,7 +1,10 @@
 ﻿using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
 using Blazor.Shared.Entities.Constants;
 using Blazor.Shared.Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Components.Forms;
 using Polly;
 
 namespace Blazor.Presentation.Client.Services;
@@ -26,5 +29,43 @@ public sealed class ImageService
         
         return await retryPolicy.ExecuteAndCaptureAsync(async () => await httpClient.GetFromJsonAsync<ImageDto>(
             $"CarouselItem/{carouselItemId}/Image"));
+    }
+    
+    public async Task<string> UploadImageAsync(int carouselItemId, IBrowserFile browserFile)
+    {
+        var httpClient = _httpClientFactory.CreateClient("Default");
+        var retryPolicy = Policy<HttpResponseMessage>
+            .Handle<HttpRequestException>()
+            .WaitAndRetryAsync(Constants.MaxHttpRequestRetries,times => TimeSpan.FromMilliseconds(times * 100));
+        
+        var buffers = new byte[browserFile.Size];
+        _ = await browserFile.OpenReadStream(browserFile.Size).ReadAsync(buffers);
+        var imageSource = $"data:{browserFile.ContentType};base64,{Convert.ToBase64String(buffers)}";
+
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var byteArrayContent = new ByteArrayContent(buffers);
+        byteArrayContent.Headers.ContentType = MediaTypeHeaderValue.Parse(browserFile.ContentType);
+
+        var policyResult = await retryPolicy.ExecuteAndCaptureAsync(async () => await httpClient.PostAsync(
+            $"CarouselItem/{carouselItemId}/Image", new MultipartFormDataContent
+        {
+            {
+                new StringContent(Path.GetFileNameWithoutExtension(browserFile.Name)), 
+                "\"fileName\""
+            },
+            {
+                byteArrayContent, 
+                "\"imageFile\"", 
+                $"\"{browserFile.Name}\""
+            }
+        }));
+
+        if (policyResult.Outcome == OutcomeType.Successful && policyResult.Result.IsSuccessStatusCode)
+        {
+            return imageSource;
+        }
+        
+        return imageSource;
     }
 }
